2f327cc3b92b456c5e2ebfc9a9379751b3380617
HimanshuMahto <k.himanshu2002@gmail.com>
2024-10-22 17:45:11 +0530

    pr checking

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
-      {/* checking PR */}
+      {/* checking PR's */}
       <div className="section">
         <EventsSection />
         <AboutSection />
