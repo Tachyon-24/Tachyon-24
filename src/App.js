e7f26077d9f3d37922ee0c90162823f17824712b
PushkarOM <pushkarchaturvedi42@gmail.com>
2024-10-28 17:28:21 +0530

    fixed the header for card and sponsors page.

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ import EventsSection from "./components/EventsSection";
 import AboutSection from "./components/AboutSection";
 import Footer from "./components/Footer";
 import Card1 from "./pages/card-1";   //adding the card's page
+import Sponsorspage from "./pages/sponsors";
+import FAB from "./components/FAB";
+import TeamPage from "./pages/TeamPage";
+import EventsPage from "./pages/EventsPage";
+import AboutPage from "./pages/AboutPage";
 
+// Scroll to top functionality
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
@@ -34,6 +48,10 @@ function App() {
           
           {/* Individual page routes */}
           <Route path="/card-1" element={<Card1 />} />
+          <Route path="/sponsors" element={<Sponsorspage />} />
+          <Route path="/team" element={<TeamPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
         <FAB />
       </div>
