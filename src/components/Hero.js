c571635c88c02954522fa93b7c589e14e39252f0
Vansh Verma <vvansh739@gmail.com>
2024-10-30 00:21:58 +0530

    pushing hr fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -98,7 +98,7 @@ const Hero = () => {
         </div>
         <div className="horizontal-line"></div>
         <div className="icon-show">
-          <div className="icon-background">
+          <div className="icon-background" style={{marginBottom:'8vw'}}>
             <img src={temple} alt="Icon" className="icon-center" />
           </div>
         </div>
