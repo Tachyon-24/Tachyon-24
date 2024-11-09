c58afa0c1a98559b2fb35ddae37aa5e918ee3ff0
Adi <adityavyas670@gmail.com>
2024-11-10 03:00:32 +0530

    home page update

--- src/components/EventsSection.js
+++ src/components/EventsSection.js
@@ -5,16 +5,6 @@ const EventsSection = () => {
   return (
     <section className="events" id="events">
       <h1>Our Events</h1>
-      <div className="events-grid">
-        <div className="event-card">
-          <h3>Games</h3>
-          <p>Coming Soon...</p>
-        </div>
-        <div className="event-card">
-          <h3>Workshops</h3>
-          <p>Coming Soon...</p>
-        </div>
-      </div>
     </section>
   );
 };
